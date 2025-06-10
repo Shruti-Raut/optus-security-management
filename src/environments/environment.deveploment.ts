98b4e12d7f24df73c0c678be7352aa2d54604c34
Shruti Raut <>
2025-06-10 18:17:56 +0530

    changes

--- src/environments/environment.deveploment.ts
+++ src/environments/environment.deveploment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     appConstants: {
       //PROD API URL's
-      // appUrl: 'https://portal.huhoka.com',
+      appUrl: 'https://portal.huhoka.com',
       marketingApi:'https://dev-marketing-agent-model-api.huhoka.com',
       
      
